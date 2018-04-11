67acba6ba4c24cbab2596a3beea88b1a08da64f4
L87R78 <L87R78@gmail.com>
2018-04-11 22:57:27 +0300

    removed folder

--- Exercises/jQuery/04 Link Buttons/buttons.js
+++
@@ -1,9 +0,0 @@
-function attachEvents() {
-
-    $('.button').on('click', function () {
-        let current  = $(this)
-        //console.log(current.hasClass)
-        $('.selected').removeClass('selected')
-        current.addClass('selected')
-    })
-}
