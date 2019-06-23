09e3b6fb4aa4ef17ce4b61916d084cbf1617d031
L87R78 <37410469+L87R78@users.noreply.github.com>
2019-06-23 18:37:17 +0300

    more exercises

---
+++ Funtions-Prototype/create with new/new.js
@@ -0,0 +1,24 @@
+
+// {}
+// obj.prototype = Human.prototype
+// Human.call(obj)
+
+
+
+function Human(name, address) {
+    this.name = name || 'Pesho'
+    this.address = address
+    this.walk = function(){
+        console.log(this.name)
+    }
+}
+
+Human.prototype.sleep = function(){
+    console.log('sleep')
+}
+
+let obj1 = new Human('Lubomir', 'Sofia')
+let obj2 = new Human('Petkan', 'Vraca')
+
+console.log(obj1)
+// console.log(obj2)
